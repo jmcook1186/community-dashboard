955f24cf8c200a2efeabf1b53e9b184f3ffb8d52
Joe <jmcook1186@gmail.com>
2023-06-28 11:59:23 +0100

    add discord activity chart

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react'
 import { ChakraProvider, SimpleGrid } from '@chakra-ui/react'
 import { Line } from 'react-chartjs-2'
 import {
@@ -39,20 +40,27 @@ const headers = APIKEY ? ({
   "Authorization": AUTH
 }) : undefined
 export const getStaticProps = async () => {
-  const GhActiveMembersUrl = process.env.GhActiveMembersUrl;
-  const response = await fetch(GhActiveMembersUrl, { headers });
-  const data = await response.text();
+  const ghActiveMembersUrl: string = process.env.ghActiveMembersUrl as string;
+  const ghActiveMembersResponse = await fetch(ghActiveMembersUrl, { method: 'GET', headers, mode: 'cors' });
+  const githubActiveMembersData = await ghActiveMembersResponse.text();
+
+
+  const discordActiverMembersUrl: string = process.env.discordActiverMembersUrl as string;
+  const discordActiverMembersResponse = await fetch(discordActiverMembersUrl, { method: 'GET', headers, mode: 'cors' });
+  const discordActiverMembersData = await discordActiverMembersResponse.text();
+
+
 
   return {
-    props: { githubData: data }
+    props: { githubActiveMembersData: githubActiveMembersData, discordActiverMembersData: discordActiverMembersData }
   }
 }
 
 
-const GithubActivityChart = ({ githubData }) => {
+const GithubActiveMembersChart = ({ data }) => {
 
-  const data = JSON.parse(githubData)
-  const cleanedData = (data.data.attributes.view_data.data[0].data)
+  const newData = JSON.parse(data)
+  const cleanedData = (newData.data.attributes.view_data.data[0].data)
   let x: Array<string> = [];
   let y: Array<number> = [];
   for (var i: number = 0; i <= cleanedData.length - 1; i++) {
@@ -85,14 +93,52 @@ const GithubActivityChart = ({ githubData }) => {
 }
 
 
-export default function App({ githubData }) {
+const DiscordActiveMembersChart = ({ data }) => {
+
+  const newData = JSON.parse(data)
+  const cleanedData = (newData.data.attributes.view_data.data[0].data)
+  let x: Array<string> = [];
+  let y: Array<number> = [];
+  for (var i: number = 0; i <= cleanedData.length - 1; i++) {
+    const p = cleanedData[i]
+
+    try {
+      x.push(p['x']);
+      y.push(p['y']);
+    }
+    catch {
+      console.log("no data")
+    }
+  }
+  const plotData = {
+    labels: x,
+    datasets: [
+      {
+        label: "Discord Activity",
+        data: y,
+        fill: false,
+        borderColor: "#742774"
+      }
+    ]
+  };
+  return (
+    <div>
+      <Line data={plotData} />
+    </div>
+  )
+}
+
+
+
+export default function App(props) {
   return (
     <div>
       <h1>Community dashboard</h1>
       <br />
       <ChakraProvider>
         <SimpleGrid columns={[2, null, 3]} spacing='40px'>
-          <GithubActivityChart githubData={githubData} />
+          <GithubActiveMembersChart data={props.githubActiveMembersData} />
+          <DiscordActiveMembersChart data={props.githubActiveMembersData} />
         </SimpleGrid>
       </ChakraProvider>
     </div>
